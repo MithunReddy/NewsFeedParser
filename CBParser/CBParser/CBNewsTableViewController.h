be46d533f89031f24d648a883274f261c98f6f38
mithun.k07 <mithun.k07@hotmail.com>
2015-01-28 16:12:05 +0530

    Initial Commit

---
+++ CBParser/CBParser/CBNewsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CBNewsTableViewController.h
+//  CBParser
+//
+//  Created by nunc03 on 1/28/15.
+//  Copyright (c) 2015 Mithun Reddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBNewsTableViewController : UITableViewController
+
+@end
